7a3648892de67bd04ba2d25eb5d80f9697a83072
EduardoFernandes1410 <eduardo.militao1410@gmail.com>
2021-06-15 22:29:46 -0300

    #1 Página de cadastro implementada

--- FRONT-END/Feedbook-Front/src/app/app-routing.module.ts
+++ FRONT-END/Feedbook-Front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent }
 ];
 
 @NgModule({
