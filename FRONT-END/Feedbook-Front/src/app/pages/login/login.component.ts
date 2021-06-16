59b9fcd9b2d080c76d2da5fe97c65ecf5e649c97
arthurbrito99 <arthur.brito1999@outlook.com>
2021-06-16 01:06:17 -0300

    #3 camada de controle da pág. de login

--- FRONT-END/Feedbook-Front/src/app/pages/login/login.component.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
+import { loginRequested } from './../../stores/auth/auth.actions';
+import { AppState } from './../../stores/reducers';
+import { AuthEffects } from './../../stores/auth/auth.effects';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
 
-  ngOnInit() {
+  get email() { return (this.loginForm as FormGroup).controls.email; }
+  get password() { return (this.loginForm as FormGroup).controls.password; }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<AppState>,
+    private router: Router,
+    private authEffects: AuthEffects,
+  ) {}
+
+  private initForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(ufmg)\.br$/)]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  public login() {
+    if (this.loginForm.valid) {
+      this.store.dispatch(loginRequested({ user: this.loginForm.value }));
+    } else {
+      console.log(this.loginForm);
+      this.loginForm.markAllAsTouched();
+      this.loginForm.markAsDirty();
+    }
+  }
+
+  public ngOnInit(): void {
+    this.initForm();
   }
 
 }
--- FRONT-END/Feedbook-Front/src/app/pages/login/login.module.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/login/login.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class LoginModule { }
