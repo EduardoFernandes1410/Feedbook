711daaed73deb5ec06c32489b71bdccf740e0c95
EduardoFernandes1410 <eduardo.militao1410@gmail.com>
2021-06-18 20:35:30 -0300

    #8 Course-card completado

--- FRONT-END/Feedbook-Front/src/app/pages/feed/feed.module.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/feed/feed.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CourseCardComponent } from './components/course-card/course-card.component';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CourseCardComponent
   ]
 })
 export class FeedModule { }
