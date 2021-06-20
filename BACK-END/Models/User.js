cc642126a1b5a4a1ad3242176f90125c9fa666a6
Pedro Dias <pedrodias504@gmail.com>
2021-06-20 20:39:11 -0300

    Finalizada a implementação da classe 'User' (login e alteração)

--- BACK-END/Models/User.js
+++ BACK-END/Models/User.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
 
 const USER_DATA_COLUMNS = 'user_id,user_name,user_email,user_pwd';
+const USER_ID_COLUMN = 'user_id';
 const USER_TABLE = 'user_tb';
 
 class User{
@@ -9,6 +11,7 @@ class User{
         this.name = name;
         this.email = email;
         this.pwdHash = passwordHash;
+        this.updated = false;
         this.dbController = dbController;
     }
 
@@ -24,8 +27,13 @@ class User{
         return new User(userData[0].user_id, userData[0].user_name, userData[0].user_email, userData[0].user_pwd, dbController);
     }
 
-    // Adds an user with the given name, email and password hash in the database, using the given controller
-    static async createNewUser(dbController, name, email, passwordHash){
+    /*
+        Adds an user with the given name, email and password hash in the 
+        database, using the given controller. Returns a User object with
+        the given data on success.
+    */
+    static async createNewUser(dbController, name, email, password){
+        const passwordHash = await bcrypt.hash(password, 12);
         let newUser = new User(null, name, email, passwordHash, dbController);
         await dbController.insert(USER_TABLE, [newUser.email, newUser.pwdHash, newUser.name], "user_email,user_pwd,user_name");
         const id = await dbController.query("SELECT last_insert_id()");
@@ -33,11 +41,63 @@ class User{
         return newUser
     }
 
+    // Deletes the user with the given id of the database, using the given controller
+    static async deleteUser(dbController, id){
+        await dbController.deleteEntry(USER_TABLE, USER_ID_COLUMN, id);
+    }
+
     // Returns the unique identifier of the current user
     getId(){
         return this.id;
     }
 
-}
+    /*
+        Validates the password for the current user. Returns true if the
+        given password matches with the current user's password and 
+        false otherwise.
+    */
+    validatePassword(password){
+        return bcrypt.compareSync(password, this.pwdHash);
+    }
+
+    // Gets the name of the current user
+    getName(){
+        return this.name;
+    }
+
+    // Gets the email of the current user
+    getEmail(){
+        return this.email;
+    }
+
+    // Updates the current user's data on the database if any changes happened
+    async saveUpdates(){
+        if(this.updated){
+            await this.dbController.updateEntry(USER_TABLE, USER_ID_COLUMN, this.id, {
+                user_name: this.name,
+                user_email: this.email,
+                user_pwd: this.pwdHash
+            });
+        }
+    }
+
+    // Updates the name of the current user with the given name (only on the object)
+    changeName(newName){
+        this.updated = true;
+        this.name = newName;
+    }
+
+    // Updates the email of the current user with the given email (only on the object)
+    changeEmail(newEmail){
+        this.updated = true;
+        this.email = newEmail;
+    }
+
+    // Updates the password (hash) of the current user with the given password (only on the object)
+    changePassword(newPassword){
+        this.updated = true;
+        this.pwdHash = bcrypt.hashSync(newPassword, 12);
+    }
+}   
 
 module.exports = User;
