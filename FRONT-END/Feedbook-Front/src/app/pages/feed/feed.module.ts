ef96aec7f5b375711cd844355cc9af83a522ee9a
arthurbrito99 <arthur.brito1999@outlook.com>
2021-06-21 11:26:56 -0300

    Aviso quando as senhas não conferem

--- FRONT-END/Feedbook-Front/src/app/pages/feed/feed.module.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/feed/feed.module.ts
@@ -1,12 +1,12 @@
+import { CourseCardComponent } from './../../components/course-card/course-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CourseCardComponent } from './components/course-card/course-card.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CourseCardComponent
+    CourseCardComponent,
   ]
 })
 export class FeedModule { }
