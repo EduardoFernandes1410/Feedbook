ce8d1d58bd238657a766b170d855d122897ec766
augustomaillo <augusto.maillo@gmail.com>
2021-06-26 20:20:09 -0300

    Criados endpoints associados ao feed e a classe que encapsula as disciplinas.

--- BACK-END/Controllers/endpoints.js
+++ BACK-END/Controllers/endpoints.js
@@ -1,11 +1,13 @@
 const User = require("../Models/User.js");
 const Evaluation = require("../Models/Evaluation.js");
+const Subject = require("../Models/Subject.js");
 
 const LOGIN_ERROR_MSG = "Wrong username or password provided.";
 const REGISTER_ERROR_MSG = "Failed to create new user.";
 const UNEXPECTED_ERROR_MSG = "unexpected error";
 const CONNECTION_ERROR_MSG = "connection error"
 const ERROR_KEY = "error";
+const AUTH_FAILURE_MSG = "authentication failure"
 
 
 /*
@@ -16,7 +18,7 @@ const ERROR_KEY = "error";
     of the database controller or authentication controllers might be defined 
     inside of this function to have easy access to those controllers. 
 */
-function registerEndpoints(app, dbController, authController){
+function registerEndpoints(app, dbController, authController) {
     // Defining the endpoint handler functions
     async function doLogin(req, res) {
         const reqBody = req.body;
@@ -24,9 +26,9 @@ function registerEndpoints(app, dbController, authController){
         // Setting response type to JSON
         res.type('json');
         // Trying to authenticate the user
-        try{
+        try {
             const user = await User.getUserByEmail(dbController, reqBody.email);
-            if(!user.validatePassword(reqBody.password)){
+            if (!user.validatePassword(reqBody.password)) {
                 throw "Invalid password!";
             }
             resBody = {
@@ -39,24 +41,24 @@ function registerEndpoints(app, dbController, authController){
                 token: await authController.getToken(user)
             };
             res.status(200);
-        }catch{
+        } catch {
             console.error("Failed login attempt registered!");
             res.status(401);
-            resBody = {error: LOGIN_ERROR_MSG};
-        }finally{
+            resBody = { error: LOGIN_ERROR_MSG };
+        } finally {
             res.json(resBody);
             res.end();
         }
     }
 
-    async function doRegister(req, res){
+    async function doRegister(req, res) {
         const reqBody = req.body;
         let resBody = {};
         res.type('json');
-        try{
-            const newUser = await User.createNewUser(dbController, 
-                reqBody.name, 
-                reqBody.surname, 
+        try {
+            const newUser = await User.createNewUser(dbController,
+                reqBody.name,
+                reqBody.surname,
                 reqBody.email,
                 reqBody.password);
             resBody = {
@@ -69,18 +71,18 @@ function registerEndpoints(app, dbController, authController){
                 token: await authController.getToken(newUser)
             };
             res.status(201);
-        }catch(error){
+        } catch (error) {
             console.error("Failed to register a new user!\nAn error happened: ", error);
             res.status(500);
-            resBody = {error: REGISTER_ERROR_MSG};
-        }finally{
+            resBody = { error: REGISTER_ERROR_MSG };
+        } finally {
             res.json(resBody);
             res.end();
-        }   
+        }
     }
 
     async function returnEvaluations(req, res) {
-        const resData = req.body;
+        const reqData = req.body;
         // checks connection
         if (!dbController.isConnected()) {
             res.status(400).send({ ERROR_KEY: CONNECTION_ERROR_MSG })
@@ -88,12 +90,16 @@ function registerEndpoints(app, dbController, authController){
         }
 
         // auth check
+        if (!await authController.validateToken(reqData.token)) {
+            res.status(400).send({ ERROR_KEY: AUTH_FAILURE_MSG })
+            return
+        }
 
         try {
-            const q = await Evaluation.getSubjectEvaluations(dbController, resData.subjectId);
+            const q = await Evaluation.getSubjectEvaluations(dbController, reqData.subjectId);
             var evaluationsArr = Array();
             for (var e of q) {
-                await Evaluation.checkVoting(dbController, e, resData.userId);
+                await Evaluation.checkVoting(dbController, e, reqData.userId);
                 evaluationsArr.push({
                     "evaluationId": e.id,
                     "evaluationOwner": e.owner,
@@ -101,6 +107,7 @@ function registerEndpoints(app, dbController, authController){
                     "evaluationMaterialQuality": e.material_quality,
                     "evaluationProfessorEvaluation": e.professor_evaluation,
                     "evaluationContentComplexity": e.content_complexity,
+                    "evaluationGeneral": e.general_evaluation,
                     "evaluationDesc": e.desc,
                     "evaluationUpvoteCount": e.upvote_count,
                     "evaluationDownvoteCount": e.downvote_count,
@@ -108,7 +115,7 @@ function registerEndpoints(app, dbController, authController){
                     "evaluationDownvoted": e.downvoted,
                 });
             };
-            res.send({ "evaluations": evaluationsArr });
+            res.send({ "evaluations": evaluationsArr }); // falta retornar subject
             return
         } catch {
             res.status(400).send({ ERROR_KEY: UNEXPECTED_ERROR_MSG });
@@ -116,18 +123,104 @@ function registerEndpoints(app, dbController, authController){
         }
     }
 
-    // async function cadastrateUser(resData, dbController) {
-    //     if (!dbController.isConnected()) {
-    //         return { "error": "Erro de conexão" }
-    //     }
-    //     console.log(User.)
-    //     const u = User.createNewUser(dbController, resData.name, resData.email, resData.password);
-    //     return JSON.stringify(u);
-    // };
+    async function feedSubjects(req, res) {
+        const reqData = req.body;
+        // checks connection
+        if (!dbController.isConnected()) {
+            res.status(400).send({ ERROR_KEY: CONNECTION_ERROR_MSG })
+            return
+        }
+
+        // auth check
+        if (!await authController.validateToken(reqData.token)) {
+            res.status(400).send({ ERROR_KEY: AUTH_FAILURE_MSG })
+            return
+        }
+
+        try {
+            const q = await Subject.getAllSubjects(dbController);
+            var subjectsArr = Array();
+
+            for (var e of q) {
+                subjectsArr.push({
+                    "subjectId": e.id,
+                    "professorName": e.professor_name,
+                    "professorImgUrl": e.professor_img_url,
+                    "professorEmail": e.professor_email,
+                    "meanDedicationTime": e.mean_dedication_time,
+                    "meanMaterialQuality": e.mean_material_quality,
+                    "meanProfessorEvaluation": e.mean_professor_evaluation,
+                    "meanContentComplexity": e.mean_content_complexity,
+                    "meanGeneral": e.mean_general,
+                    "subjectCod": e.cod,
+                    "subjectName": e.name,
+                    "evaluationsCount": e.evaluations_count
+                });
+            };
+
+            subjectsArr.sort(function(a, b) {
+                return a[reqData.orderBy] > b[reqData.orderBy];
+            });
+
+            res.send({ "subjects": subjectsArr.reverse() });
+            return
+        } catch {
+            res.status(400).send({ ERROR_KEY: UNEXPECTED_ERROR_MSG });
+            return
+        }
+    }
+
+    async function doSearch(req, res) {
+        const reqData = req.body;
+        // checks connection
+        if (!dbController.isConnected()) {
+            res.status(400).send({ ERROR_KEY: CONNECTION_ERROR_MSG })
+            return
+        }
+
+        // auth check
+        if (!await authController.validateToken(reqData.token)) {
+            res.status(400).send({ ERROR_KEY: AUTH_FAILURE_MSG })
+            return
+        }
+
+        try {
+            const q = await Subject.searchForSubjectName(dbController, reqData.query);
+            var subjectsArr = Array();
+
+            for (var e of q) {
+                subjectsArr.push({
+                    "subjectId": e.id,
+                    "professorName": e.professor_name,
+                    "professorImgUrl": e.professor_img_url,
+                    "professorEmail": e.professor_email,
+                    "meanDedicationTime": e.mean_dedication_time,
+                    "meanMaterialQuality": e.mean_material_quality,
+                    "meanProfessorEvaluation": e.mean_professor_evaluation,
+                    "meanContentComplexity": e.mean_content_complexity,
+                    "meanGeneral": e.mean_general,
+                    "subjectCod": e.cod,
+                    "subjectName": e.name,
+                    "evaluationsCount": e.evaluations_count
+                });
+            };
+
+            res.send({ "subjects": subjectsArr });
+            return
+        } catch {
+            res.status(400).send({ ERROR_KEY: UNEXPECTED_ERROR_MSG });
+            return
+        }
+    }
 
     // Registering the endpoints
     app.post('/user/login', doLogin);
     app.post('/user/register', doRegister);
+    app.post('/subject/evaluations', returnEvaluations);
+    app.post('/feed/all', feedSubjects);
+    app.post('/feed/search', doSearch);
+
+
 }
 
 module.exports = registerEndpoints;
---
+++ BACK-END/Models/Subject.js
@@ -0,0 +1,94 @@
+const mysql = require('mysql2');
+
+const SUBJECT_DATA_COLUMNS = 'subject_id,professor_id,mean_dedication_time,mean_material_quality,mean_professor_evaluation,mean_content_complexity,mean_general,subject_cod,subject_name,evaluations_count,search_field';
+const SUBJECT_TABLE = 'subject_tb';
+const PROFESSOR_TALBE = 'professor_tb';
+const PROFESSOR_DATA_COLUMNS = 'professor_id,professor_name,professor_email,professor_img_url';
+
+class Subject {
+    constructor(id, professor_id, mean_dedication_time, mean_material_quality, mean_professor_avalaition, mean_content_complexity, mean_general, cod, name, evaluations_count, search_field, dbController = null) {
+        this.id = id;
+        this.professor_id = professor_id;
+        this.mean_dedication_time = mean_dedication_time;
+        this.mean_material_quality = mean_material_quality;
+        this.mean_professor_evaluation = mean_professor_avalaition;
+        this.mean_content_complexity = mean_content_complexity;
+        this.mean_general = mean_general
+        this.cod = cod;
+        this.name = name;
+        this.evaluations_count = evaluations_count;
+        this.search_field = search_field;
+        this.dbController = dbController;
+        this.professor_name = null;
+        this.professor_img_url = null;
+        this.professor_email = null;
+    }
+
+    // Auxiliar method to construct a Subject object from query result
+    static async subjectFromData(subjectData, dbController) {
+        var subjects = []
+        subjectData.forEach(element => {
+            subjects.push(new Subject(
+                element.subject_id,
+                element.professor_id,
+                element.mean_dedication_time,
+                element.mean_material_quality,
+                element.mean_professor_evaluation,
+                element.mean_content_complexity,
+                element.mean_general,
+                element.subject_cod,
+                element.subject_name,
+                element.evaluations_count,
+                element.search_field,
+                dbController
+            ))
+        });
+        for (var value of subjects) {
+            await Subject.getSubjectProfessor(dbController, value);
+        }
+        return subjects;
+    }
+
+    // Retrieves the data of Subject with the given id from the database, using the given controller
+    static async getSubjectById(dbController, id) {
+        const subjectData = await dbController.select(SUBJECT_TABLE, SUBJECT_DATA_COLUMNS, mysql.format('subject_id=?', id));
+        return await Subject.subjectFromData(subjectData, dbController);
+    }
+
+    // Retrieves the data of the Subject with the given name from the database, using the given controller
+    static async searchForSubjectName(dbController, name) {
+        name = name.normalize("NFD").replace(/[^0-9a-z A-Zs]/g, "");
+        name = name.toUpperCase();
+
+        var query = mysql.format("search_field LIKE ?", "%" + name + "%");
+
+        const suffix = name.split(" ");
+        for (var value of suffix) {
+            query += mysql.format(" OR search_field LIKE ?", "%" + value + "%");
+        }
+
+        const subjectData = await dbController.select(SUBJECT_TABLE, SUBJECT_DATA_COLUMNS, query);
+        return await Subject.subjectFromData(subjectData, dbController);
+    }
+
+    // Retrieves all subjects table rows as Subjects objects
+    static async getAllSubjects(dbController) {
+        const subjectData = await dbController.select(SUBJECT_TABLE, SUBJECT_DATA_COLUMNS, "");
+        return await Subject.subjectFromData(subjectData, dbController);
+    }
+
+    static async getSubjectProfessor(dbController, subject) {
+        const professorData = await dbController.select(PROFESSOR_TALBE, PROFESSOR_DATA_COLUMNS, mysql.format("professor_id=?", subject.professor_id));
+        subject.professor_name = professorData[0].professor_name;
+        subject.professor_email = professorData[0].professor_email;
+        subject.professor_img_url = professorData[0].professor_img_url;
+    }
+
+    // Returns the unique identifier of the current Subject
+    getId() {
+        return this.id;
+    }
+
+}
+
+module.exports = Subject;
