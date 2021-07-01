4187a2a0041f96fd60dd9178e5f0bf57f1a3bc02
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-01 00:38:19 -0300

    configurando os tipos das variáveis de autenticação

--- FRONT-END/Feedbook-Front/src/app/models/auth.ts
+++ FRONT-END/Feedbook-Front/src/app/models/auth.ts
@@ -2,95 +2,20 @@ export interface Credentials {
   email: string;
   password: string;
 }
-
-export interface UserNotRegistered {
-  email: string;
-  name: string;
-  picture: string;
-  userIdGoogle?: string;
-  userIdFace?: string;
-}
-
 export interface UserData {
-  client: User;
+  user: User;
   token: string;
 }
-
-export interface UserInfo {
-  email: string;
+export interface User {
   name: string;
-  password?: string;
-  picture: string;
-  pictureServerPath: string;
-  document: string;
-  cellphone: string;
-  userIdFace?: string;
-  userIdGoogle?: string;
-}
-
-export interface AccountType {
-  code: string;
-  description: string;
+  surname: string;
+  email: string;
   id: number;
 }
 
-export interface Address {
-  id: string;
-  cep: string;
-  publicPlace?: any;
-  street: string;
-  observation: string;
-  number: string;
-  neighborhood: string;
-  city: string;
-  state: string;
-
-  stopId?: number;
-}
-
-export interface UserToRegister {
+export interface RegisterData {
   name: string;
+  surname: string;
   email: string;
   password: string;
-  cellphone: string;
-}
-
-export interface FacebookRes {
-  authResponse: {
-    accessToken: string;
-    expiresIn: string;
-    session_key: true,
-    sig: string;
-    userID: string;
-  };
-  status: string;
-}
-
-export interface GoogleRes {
-  accessToken: string;
-  displayName: string;
-  email: string;
-  expires: number;
-  expires_in: string;
-  familyName: string;
-  givenName: string;
-  imageUrl: string;
-  userId: string;
-}
-
-export interface LoggedUser {
-  token: string;
-  student: User;
-}
-
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  document?: any;
-  pmNumber?: any;
-  picture?: any;
-  phone: string;
-  createdAt: string;
-  pictureServerPath?: any;
 }
