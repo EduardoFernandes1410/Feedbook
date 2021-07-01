2874c3c72cec77d5e942ddbdcec681cfca30c81c
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-01 01:40:30 -0300

    atualizando importações

--- FRONT-END/Feedbook-Front/src/app/stores/effects.ts
+++ FRONT-END/Feedbook-Front/src/app/stores/effects.ts
@@ -1,5 +1,7 @@
+import { FeedEffects } from './feed/feed.effects';
 import { AuthEffects } from './auth/auth.effects';
 
 export const effects = [
   AuthEffects,
+  FeedEffects,
 ];
--- FRONT-END/Feedbook-Front/src/app/stores/reducers.ts
+++ FRONT-END/Feedbook-Front/src/app/stores/reducers.ts
@@ -1,14 +1,18 @@
+import { FeedState } from './feed/feed.state';
 import { ActionReducerMap, createSelector } from '@ngrx/store';
 import { authReducer } from './auth/auth.reducer';
 import { AuthState } from './auth/auth.state';
+import { feedReducer } from './feed/feed.reducer';
 
 // tslint:disable-next-line: no-empty-interface
 export interface AppState {
   auth: AuthState;
+  feed: FeedState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
   auth: authReducer,
+  feed: feedReducer,
 };
 
 export const mapApplicationState = (state: AppState) => state;
