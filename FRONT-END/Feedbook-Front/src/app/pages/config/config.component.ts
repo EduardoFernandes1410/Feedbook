aeb952b43e039851e2ecf9e3008874730eae530b
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-03 00:33:48 -0300

    #5 controle da pag de config

--- FRONT-END/Feedbook-Front/src/app/pages/config/config.component.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/config/config.component.ts
@@ -1,4 +1,12 @@
+import { first } from 'rxjs/operators';
+import { getLoggedUser } from './../../stores/auth/auth.selectors';
+import { UserData } from './../../models/auth';
+import { userUpdateRequested } from './../../stores/auth/auth.actions';
+import { AppState } from './../../stores/reducers';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { MustMatch } from 'src/app/validators/password.validator';
 
 @Component({
   selector: 'app-config',
@@ -7,7 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigComponent implements OnInit {
 
-  constructor() { }
+  public updateForm: FormGroup;
+  public loggedUser: UserData;
+
+  get name() { return (this.updateForm as FormGroup).controls.name; }
+  get surname() { return (this.updateForm as FormGroup).controls.surname; }
+  get email() { return (this.updateForm as FormGroup).controls.email; }
+  get password() { return (this.updateForm as FormGroup).controls.password; }
+  get passwordConfirmation() { return (this.updateForm as FormGroup).controls.passwordConfirmation; }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<AppState>,
+  ) { }
+
+  private initForm(): void {
+    // tslint:disable-next-line: deprecation
+    this.updateForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(ufmg)\.br$/)]],
+      password: ['', [Validators.required]],
+      passwordConfirmation: ['', [Validators.required]],
+    },
+    {
+      validator: MustMatch('password', 'passwordConfirmation'),
+    });
+  }
+
+  public async update() {
+    if (this.updateForm.valid) {
+      this.loggedUser = await this.store.select(getLoggedUser).pipe(first()).toPromise();
+
+      this.store.dispatch(userUpdateRequested({ userData: this.updateForm.value, token: this.loggedUser.token }));
+    } else {
+      console.log(this.updateForm);
+      this.updateForm.markAllAsTouched();
+      this.updateForm.markAsDirty();
+    }
+  }
 
   ngOnInit() {
   }
