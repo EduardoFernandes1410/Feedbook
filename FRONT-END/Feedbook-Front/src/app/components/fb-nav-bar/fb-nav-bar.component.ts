7e609f9ae0761332f2c52619796e12c55900ec06
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-03 12:55:52 -0300

    controle de usuário logado na nav-bar

--- FRONT-END/Feedbook-Front/src/app/components/fb-nav-bar/fb-nav-bar.component.ts
+++ FRONT-END/Feedbook-Front/src/app/components/fb-nav-bar/fb-nav-bar.component.ts
@@ -1,4 +1,11 @@
+import { first } from 'rxjs/operators';
+import { getLoggedUser } from './../../stores/auth/auth.selectors';
+import { AppState } from './../../stores/reducers';
+import { UserData } from './../../models/auth';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { logout } from 'src/app/stores/auth/auth.actions';
 
 @Component({
   selector: 'app-fb-nav-bar',
@@ -6,12 +13,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./fb-nav-bar.component.scss']
 })
 export class FbNavBarComponent implements OnInit {
+
   public menuVisible: boolean;
+  public loggedUser$: Observable<UserData>;
+
   @Input() searchBox: boolean;
   @Input() configButton: boolean;
   @Output() whenSearch: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>,
+  ) { }
 
   public toggleMenu(): void {
     this.menuVisible = !this.menuVisible;
@@ -21,7 +33,12 @@ export class FbNavBarComponent implements OnInit {
     this.whenSearch.emit(event.target.value);
   }
 
-  ngOnInit() {
+  public logout() {
+    this.store.dispatch(logout());
+  }
+
+  async ngOnInit() {
     this.menuVisible = false;
+    this.loggedUser$ = this.store.select(getLoggedUser);
   }
 }
