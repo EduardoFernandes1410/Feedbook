cb3c1cb5791272d591a5365f89c0cd4b16bd5bb0
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-04 05:14:57 -0300

    #9 cartões com informação dinamica no feed

--- FRONT-END/Feedbook-Front/src/app/components/course-card/course-card.component.ts
+++ FRONT-END/Feedbook-Front/src/app/components/course-card/course-card.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { getLoggedUser } from './../../stores/auth/auth.selectors';
+import { UserData } from './../../models/auth';
+import { AppState } from './../../stores/reducers';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { SubjectItem } from 'src/app/models/feed';
+import { Store } from '@ngrx/store';
+import { evaluationsRequested } from 'src/app/stores/subject/subject.actions';
 
 @Component({
   selector: 'app-course-card',
@@ -7,7 +15,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseCardComponent implements OnInit {
 
-  constructor() { }
+  @Output() whenSeeEvaluations: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() subject: SubjectItem;
+  public loggedUser: UserData;
+
+  constructor(
+    private router: Router,
+    private store: Store<AppState>,
+  ) { }
+
+  public async seeEvaluations(){
+    this.loggedUser = await this.store.select(getLoggedUser).pipe(first()).toPromise();
+    this.store.dispatch(evaluationsRequested({ userId: this.loggedUser.user.id, subjectId: this.subject.subjectId, token: this.loggedUser.token }));
+    this.whenSeeEvaluations.emit(true);
+  }
+
+  public goToEvaluation(){
+    this.router.navigate([`/rating/${this.subject.subjectId}`]);
+
+  }
 
   ngOnInit() {
   }
