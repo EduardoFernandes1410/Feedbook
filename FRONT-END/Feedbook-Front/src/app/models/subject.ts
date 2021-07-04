ea48d3fa866e67022f717fd1223643e19bfb6b84
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-04 05:16:46 -0300

    configs necessárias para avaliações

--- FRONT-END/Feedbook-Front/src/app/models/subject.ts
+++ FRONT-END/Feedbook-Front/src/app/models/subject.ts
@@ -6,5 +6,20 @@ export interface Evaluation {
   evaluation_content_complexity: number;
   general_evaluation: number;
   evaluation_desc: string;
+}
+
+export interface EvaluationData {
+  evaluation_id: string;
+  evaluation_owner: string;
+  evaluation_dedication_time: number;
+  evaluation_material_quality: number;
+  evaluation_professor_evaluation: number;
+  evaluation_content_complexity: number;
+  general_evaluation: number;
+  evaluation_desc: string;
+  evaluation_upvote_count: number;
+  evaluation_downvote_count: number;
+  evaluation_upvoted: boolean;
+  evaluation_downvoted: boolean;
 
 }
