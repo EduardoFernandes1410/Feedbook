335252b7452954e7777015abab54ff195172ae18
augustomaillo <augusto.maillo@gmail.com>
2021-07-04 13:43:20 -0300

    Ajustes

--- BACK-END/app.js
+++ BACK-END/app.js
@@ -12,6 +12,7 @@ const app = express();
 // Adding the JSON body parser to the app
 app.use(cors());
 app.use(express.json());
+app.use(express.static('images'));
 app.use(function(req, res, next) {
 
     // Website you wish to allow to connect
