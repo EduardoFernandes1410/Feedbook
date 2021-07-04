e67f7bb50ca0cdf76874460e6d5ab7b9c23eec03
arthurbrito99 <arthur.brito1999@outlook.com>
2021-07-04 05:17:35 -0300

    #9 pagina de feed com quantidade de cartões dinamica

--- FRONT-END/Feedbook-Front/src/app/pages/feed/feed.component.ts
+++ FRONT-END/Feedbook-Front/src/app/pages/feed/feed.component.ts
@@ -1,8 +1,14 @@
+import { first } from 'rxjs/operators';
+import { getLoggedUser } from './../../stores/auth/auth.selectors';
+import { UserData } from './../../models/auth';
+import { Observable } from 'rxjs';
+import { FeedEffects } from './../../stores/feed/feed.effects';
 import { getSubjectList } from './../../stores/feed/feed.selectors';
 import { AppState } from 'src/app/stores/reducers';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { getSubjectRequested, subjectListRequested } from 'src/app/stores/feed/feed.actions';
+import { SubjectList } from 'src/app/models/feed';
 
 @Component({
   selector: 'app-feed',
@@ -10,21 +16,41 @@ import { getSubjectRequested, subjectListRequested } from 'src/app/stores/feed/f
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
+
   public orderBy: string = 'meanGeneral';
+  public visible: boolean = false;
+  public subjectList$: Observable<SubjectList>;
+  public loggedUser: UserData;
 
   constructor(
     private store: Store<AppState>,
+    private feedEffects: FeedEffects,
   ) {}
 
   public searchSubject(text: string){
-    this.store.dispatch(getSubjectRequested({ query: text }));
+    this.store.dispatch(getSubjectRequested({ query: text, token: this.loggedUser.token }));
   }
 
   public getSubjectList(){
-    this.store.dispatch(subjectListRequested({ orderBy: this.orderBy }));
+    this.store.dispatch(subjectListRequested({ orderBy: this.orderBy, token: this.loggedUser.token }));
   }
 
-  ngOnInit() {
-    this.store.dispatch(subjectListRequested({ orderBy: this.orderBy }));
+  public seeEvaluations(value: boolean){
+    console.log(value);
+    this.visible = true;
+  }
+
+  public closedEvaluations(value: boolean) {
+    console.log('close');
+    this.visible = false;
+  }
+
+  async ngOnInit() {
+    this.subjectList$ = this.store.select(getSubjectList);
+    this.loggedUser = await this.store.select(getLoggedUser).pipe(first()).toPromise();
+    this.store.dispatch(subjectListRequested({ orderBy: this.orderBy, token: this.loggedUser.token }));
+    this.feedEffects.subjectListCompleted$.subscribe((data) => {
+      console.log(data);
+    });
   }
 }
